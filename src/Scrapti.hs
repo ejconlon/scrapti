e7a5c3369315042c93e4b7718b59681842b4d77f
Eric Conlon <ejconlon@gmail.com>
2022-06-22 11:55:01 -0700

    no sdl

--- src/Scrapti.hs
+++ src/Scrapti.hs
@@ -1,26 +1,29 @@
 module Scrapti where
 
-import Control.Monad.Trans.Resource (MonadResource, allocate, runResourceT)
-import qualified Data.ByteString.Lazy as BSL
-import Scrapti.Binary (decodeFail)
-import Scrapti.Sample (Sampled)
-import Scrapti.Wav (Wav, decodeAnyWav)
-import qualified SDL
+-- import Control.Monad.Trans.Resource (MonadResource, allocate, runResourceT)
+-- import qualified Data.ByteString.Lazy as BSL
+-- import Scrapti.Binary (decodeFail)
+-- import Scrapti.Sample (Sampled)
+-- import Scrapti.Wav (Wav, decodeAnyWav)
+-- import qualified SDL
 
 exe :: IO ()
-exe = do
-  bs <- BSL.readFile "testdata/drums.wav"
-  swav <- decodeFail bs decodeAnyWav
-  playSound swav
+exe = pure ()
 
-playSound :: Sampled Wav -> IO ()
-playSound _swav = runResourceT $ do
-  sdlInit
+-- exe :: IO ()
+-- exe = do
+--   bs <- BSL.readFile "testdata/drums.wav"
+--   swav <- decodeFail bs decodeAnyWav
+--   playSound swav
 
-alloc :: MonadResource m => IO a -> (a -> IO ()) -> m a
-alloc allocRes freeRes = fmap snd (allocate allocRes freeRes)
+-- playSound :: Sampled Wav -> IO ()
+-- playSound _swav = runResourceT $ do
+--   sdlInit
 
-sdlInit :: MonadResource m => m ()
-sdlInit = alloc x y where
-  x = SDL.initialize [SDL.InitAudio]
-  y = const SDL.quit
+-- alloc :: MonadResource m => IO a -> (a -> IO ()) -> m a
+-- alloc allocRes freeRes = fmap snd (allocate allocRes freeRes)
+
+-- sdlInit :: MonadResource m => m ()
+-- sdlInit = alloc x y where
+--   x = SDL.initialize [SDL.InitAudio]
+--   y = const SDL.quit
