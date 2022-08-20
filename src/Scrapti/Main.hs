ff075eadaf05623b6ab74c475655f60efc0d2908
Eric Conlon <ejconlon@gmail.com>
2022-08-19 18:18:06 -0700

    stub exe

--- src/Scrapti/Main.hs
+++ src/Scrapti/Main.hs
@@ -1,4 +1,15 @@
 module Scrapti.Main (main) where
+import System.Exit (die)
+import System.Environment (getArgs)
+
+convert :: FilePath -> FilePath -> IO ()
+convert _aiffPath _wavPath = do
+  putStrLn "TODO"
 
 main :: IO ()
-main = pure ()
+main = do
+  args <- getArgs
+  case args of
+    ["convert-aif", aifPath, wavPath] ->
+      convert aifPath wavPath
+    _ -> die "no dice"
