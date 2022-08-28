fe1f1326bf5e2280c7b1516ea0627d2a32035bbe
Eric Conlon <ejconlon@gmail.com>
2022-08-28 11:13:46 -0700

    finish sample loader

--- src/Scrapti/Patch.hs
+++ src/Scrapti/Patch.hs
@@ -1,43 +1,19 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Scrapti.Patch where
-import Control.Monad (unless)
-import Data.Default (Default (..))
-import Data.Maybe (isJust, isNothing)
-import Data.Sequence (Seq)
-import Data.Text (Text)
-import Scrapti.Common (LoopMarkNames, LoopMarkPoints)
-import Scrapti.Convert (readPtiWav)
-import Scrapti.Patches.Inst (InstSpec)
-import Scrapti.Tracker.Pti (Header, InstParams, Pti, mkPti)
 
-data Sample = Sample
-  { samplePath :: !FilePath
-  , sampleNote :: !Int
-  , sampleVel :: !(Maybe Int)
-  , sampleUniq :: !(Maybe Text)
-  } deriving stock (Eq, Show)
+-- data PtiInst = PtiInst !Text !Text !Pti
+--   deriving (Eq, Show)
 
-findSamples :: Text -> FilePath -> IO (Seq Sample)
-findSamples prefix dir = error "TODO"
+-- instToHeader :: Maybe LoopMarkPoints -> Int -> InstParams -> Either String Header
+-- instToHeader mayPoints noteOffset params = do
+--   let !hd = def @Header
+--   error "TODO"
 
-data PtiInst = PtiInst !Text !Text !Pti
-  deriving (Eq, Show)
+-- instToPti :: Maybe LoopMarkNames -> Text -> Text -> Int -> InstParams -> Sample -> IO Pti
+-- instToPti mayNames instName noteName noteCenterVal params sample = do
+--   (arr, mayPoints) <- readPtiWav mayNames (samplePath sample)
+--   unless (isNothing mayNames || isJust mayPoints) (fail "did not find expected loop points")
+--   let !header = undefined
+--   pure $! mkPti header arr
 
-instToHeader :: Maybe LoopMarkPoints -> Int -> InstParams -> Either String Header
-instToHeader mayPoints noteOffset params = do
-  let !hd = def @Header
-  error "TODO"
-
-instToPti :: Maybe LoopMarkNames -> Text -> Text -> Int -> InstParams -> Sample -> IO Pti
-instToPti mayNames instName noteName noteCenterVal params sample = do
-  (arr, mayPoints) <- readPtiWav mayNames (samplePath sample)
-  unless (isNothing mayNames || isJust mayPoints) (fail "did not find expected loop points")
-  let !header = undefined
-  pure $! mkPti header arr
-
-instToPtis :: InstSpec FilePath -> Seq Sample -> IO (Seq PtiInst)
-instToPtis = error "TODO"
-
--- instToSfz :: InstSpec -> Seq Sample -> IO SfzFile
--- instToSfz = error "TODO"
+-- instToPtis :: InstSpec FilePath -> Seq Sample -> IO (Seq PtiInst)
+-- instToPtis = error "TODO"
---
+++ src/Scrapti/Patches/Loader.hs
@@ -0,0 +1,60 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Scrapti.Patches.Loader where
+
+import Scrapti.Midi.Notes (OctNote, parseNote)
+import Scrapti.Midi.Msg (Velocity (..))
+import Data.Text (Text)
+import qualified Data.Text as T
+import System.FilePath ((</>))
+import System.Directory (listDirectory)
+import Text.Regex.TDFA ((=~~))
+import Text.Read (readMaybe)
+import Data.Maybe (maybeToList)
+
+data Sample = Sample
+  { samplePath :: !FilePath
+  , sampleNote :: !OctNote
+  , sampleVel :: !(Maybe Velocity)
+  , sampleUniq :: !(Maybe Text)
+  } deriving stock (Eq, Show)
+
+newtype F a = F { unF :: Either String a }
+  deriving newtype (Functor, Applicative, Monad)
+
+instance MonadFail F where
+  fail = F . Left
+
+matchFiles :: Text -> Text -> FilePath -> IO [(FilePath, [Text])]
+matchFiles prefix fileExt dir = do
+  let pat = "^" ++ T.unpack prefix ++ "([^\\\\\\.]+)\\." ++ T.unpack fileExt ++ "$"
+  files <- listDirectory dir
+  pure $! do
+    fp <- files
+    let ex = fp =~~ pat :: F (String, String, String, [String])
+    case unF ex of
+      Right (_, _, _, [dashed]) ->
+        let parts = T.splitOn "-" (T.pack dashed)
+        in [(dir </> fp, parts)]
+      _ -> []
+
+parseVel :: Text -> Maybe Velocity
+parseVel v
+  | not (T.null v) && T.head v == 'V' = fmap Velocity (readMaybe (T.unpack v))
+  | otherwise = Nothing
+
+parseSample :: FilePath -> [Text] -> Maybe Sample
+parseSample fp parts = do
+  (noteStr, mayVelStr, mayUniqStr) <- case parts of
+    [noteStr] -> Just (noteStr, Nothing, Nothing)
+    [noteStr, velStr] -> Just (noteStr, Just velStr, Nothing)
+    [noteStr, velStr, uniqStr] -> Just (noteStr, Just velStr, Just uniqStr)
+    _ -> Nothing
+  octNote <- parseNote noteStr
+  let mayVel = mayVelStr >>= parseVel
+  pure $! Sample fp octNote mayVel mayUniqStr
+
+matchSamples :: Text -> Text -> FilePath -> IO [Sample]
+matchSamples prefix fileExt dir = do
+  pairs <- matchFiles prefix fileExt dir
+  pure $! pairs >>= maybeToList . uncurry parseSample
