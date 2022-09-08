23d15a9a7146914172e3580b46f05eb0e12880b3
Eric Conlon <ejconlon@gmail.com>
2022-09-08 14:27:25 -0700

    json

--- src/Scrapti/Patches/Meta.hs
+++ src/Scrapti/Patches/Meta.hs
@@ -5,8 +5,12 @@ module Scrapti.Patches.Meta where
 import Data.Text (Text)
 import Data.Sequence (Seq)
 import GHC.Generics (Generic (..))
-import Data.Aeson (genericToJSON, ToJSON (..), FromJSON (..), genericParseJSON, GToJSON', Value, Zero, GFromJSON)
+import Data.Aeson (genericToJSON, ToJSON (..), FromJSON (..), genericParseJSON, GToJSON', Value, Zero, GFromJSON, eitherDecodeStrict')
 import Data.Aeson.Casing (aesonPrefix, snakeCase)
+import qualified Data.Text.Encoding as TE
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Builder as TLB
+import Data.Aeson.Encode.Pretty (encodePrettyToTextBuilder)
 
 newtype SnakeRecord a = SnakeRecord { unSnakeRecord :: a }
 
@@ -35,3 +39,9 @@ data Meta = Meta
   , mxChangelog :: !(Seq Version)
   } deriving stock (Eq, Show, Generic)
     deriving (ToJSON, FromJSON) via (SnakeRecord Meta)
+
+metaToJson :: Meta -> Text
+metaToJson = TL.toStrict . TLB.toLazyText . encodePrettyToTextBuilder
+
+jsonToMeta :: Text -> Either String Meta
+jsonToMeta = eitherDecodeStrict' . TE.encodeUtf8
